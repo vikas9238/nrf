890c408e644d3f192e2c6a3ea677c157462911ef
vikas9238 <vikashkumar9238@gmail.com>
2024-06-20 21:44:34 +0530

    /nav

--- inc/footer.php
+++ inc/footer.php
@@ -134,7 +134,7 @@
 
 <!-- JavaScript Libraries -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url ?>libs/wow/wow.min.js"></script>
 <script src="<?php echo base_url ?>libs/easing/easing.min.js"></script>
 <script src="<?php echo base_url ?>libs/waypoints/waypoints.min.js"></script>
