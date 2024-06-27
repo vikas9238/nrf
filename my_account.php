f6c2942f88b25588ceb6f7c9f8d3b460accc4f54
vikas9238 <vikashkumar9238@gmail.com>
2024-06-28 03:16:42 +0530

    user account

--- my_account.php
+++ my_account.php
@@ -1,17 +1,18 @@
-    
-<style>
-    .badge-light{
-        color:black
-    }
-</style>
-<section class="py-2">
-    <div class="container">
-        <div class="card rounded-0">
-            <div class="card-body">
-                <div class="w-100 justify-content-between d-flex">
-                    <h4><b>My Bookings</b></h4>
-                    <a href="./?p=edit_account" class="btn btn btn-dark btn-flat"><div class="fa fa-user-cog"></div> Manage Account</a>
-                </div>
+    <style>
+        .badge-light {
+            color: black
+        }
+    </style>
+    <section class="py-2">
+        <div class="container">
+            <div class="card rounded-0">
+                <div class="card-body">
+                    <div class="w-100 justify-content-between d-flex">
+                        <h4><b>My Bookings</b></h4>
+                        <a href="./?p=edit_account" class="btn btn btn-dark btn-flat">
+                            <div class="fa fa-user-cog"></div> Manage Account
+                        </a>
+                    </div>
                     <hr class="border-warning">
                     <table class="table table-stripped text-dark">
                         <colgroup>
@@ -26,64 +27,72 @@
                             <tr class="bg-navy text-white">
                                 <th>#</th>
                                 <th>Date Booked</th>
-                                <th>Rent Schedule</th>
-                                <th>Client</th>
+                                <th>Booking Details</th>
+                                <th>Company Details</th>
                                 <th>Status</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php 
-                                $i = 1;
-                                $qry = $conn->query("SELECT r.*,concat(c.firstname,' ',c.lastname) as client from `booking_list` r inner join clients c on c.id = r.client_id where client_id = '{$_settings->userdata('id')}' order by unix_timestamp(r.date_created) desc ");
-                                while($row = $qry->fetch_assoc()):
+                            <?php
+                            $i = 1;
+                            $qry = $conn->query("SELECT r.*,q.address,q.description,c.name,p.category from `booking_list` r inner join quotation_list q on q.id = r.quotation_id inner join company_list c on q.company_id=c.id inner join product p on p.id=q.product_id where client_id = '{$_settings->userdata('id')}' order by unix_timestamp(r.date_created) desc ");
+                            while ($row = $qry->fetch_assoc()) :
                             ?>
-                            <tr>
-                                <td class="text-center"><?php echo $i++; ?></td>
-                                <td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
-                                <td>
-                                    <small><span class="text-muted">Pick up:</span><?php echo date("Y-m-d",strtotime($row['date_start'])) ?></small><br>
-                                    <small><span class="text-muted">Return: </span><?php echo date("Y-m-d",strtotime($row['date_end'])) ?></small>
-                                </td>
-                                <td><?php echo $row['client'] ?></td>
-                                <td class="text-center">
-                                    <?php if($row['status'] == 0): ?>
-                                        <span class="badge badge-light">Pending</span>
-                                    <?php elseif($row['status'] == 1): ?>
-                                        <span class="badge badge-primary">Confirmed</span>
-                                    <?php elseif($row['status'] == 2): ?>
-                                        <span class="badge badge-danger">Cancelled</span>
-                                    <?php elseif($row['status'] == 3): ?>
-                                        <span class="badge badge-warning">Picked Up</span>
-                                    <?php elseif($row['status'] == 4): ?>
-                                        <span class="badge badge-success">Returned</span>
-                                    <?php else: ?>
-                                        <span class="badge badge-danger">Cancelled</span>
-                                    <?php endif; ?>
-                                </td>
-                                <td align="center">
-                                    <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                                <tr>
+                                    <td class="text-center"><?php echo $i++; ?></td>
+                                    <td><?php echo date("Y-m-d H:i", strtotime($row['date_created'])) ?></td>
+                                    <td>
+                                        <small><span class="text-muted">PO Rate: </span><?php echo number_format($row['po_rate']) ?></small><br>
+                                        <small><span class="text-muted">Daily Rate: </span><?php echo number_format($row['daily_rate']) ?></small><br>
+                                        <small><span class="text-muted">Quantity: </span><?php echo number_format($row['quantity']) ?></small><br>
+                                        <small><span class="text-muted">Approved Quantity: </span><?php echo number_format($row['approved_quantity']) ?></small>
+                                    </td>
+                                    <td>
+                                        <small><span class="text-muted">Product: </span><?php echo $row['category'] ?></small><br>
+                                        <small><span class="text-muted">Company: </span><?php echo $row['name'] ?></small><br>
+                                        <small><span class="text-muted">Address: </span><?php echo $row['address'] ?></small><br>
+                                        <small><span class="text-muted">Description: </span><?php echo strip_tags(stripslashes(html_entity_decode($row['description']))) ?></small>
+                                    </td>
+                                    <td class="text-center">
+                                        <?php if ($row['status'] == 0) : ?>
+                                            <span class="badge badge-light">Pending</span>
+                                        <?php elseif ($row['status'] == 1) : ?>
+                                            <span class="badge badge-primary">Confirmed</span>
+                                        <?php elseif ($row['status'] == 2) : ?>
+                                            <span class="badge badge-danger">Cancelled</span>
+                                        <?php elseif ($row['status'] == 3) : ?>
+                                            <span class="badge badge-success">Active</span>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td align="center">
+                                        <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
                                             Action
-                                        <span class="sr-only">Toggle Dropdown</span>
-                                    </button>
-                                    <div class="dropdown-menu" role="menu">
-                                        <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-th-list text-dark"></span> View Details</a>
-                                    </div>
-                                </td>
-                            </tr>
+                                            <span class="sr-only">Toggle Dropdown</span>
+                                        </button>
+                                        <div class="dropdown-menu" role="menu">
+                                            <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-th-list text-dark"></span> View Details</a>
+                                            <!-- <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item transaction_data" href="javascript:void(0)" data-id="<?php echo $row['quotation_id'] ?>"><span class="fa fa-th-list text-dark"></span> Transaction Details</a> -->
+                                        </div>
+                                    </td>
+                                </tr>
                             <?php endwhile; ?>
                         </tbody>
                     </table>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<script>
-    $(function(){
-        $('.view_data').click(function(){
-            uni_modal("Order Details","./admin/bookings/view_booking.php?view=user&id="+$(this).attr('data-id'),'large')
-        })
-        $('table').dataTable();
+    </section>
+    <script>
+        $(function() {
+            $('.view_data').click(function() {
+                uni_modal("Order Details", "./admin/bookings/view_booking.php?view=user&id=" + $(this).attr('data-id'), 'large')
+            })
+            // $('.transaction_data').click(function() {
+            //      uni_modal("Order Details", "./admin/maintenance/transaction.php?view=user&client_id="+<?php echo $_settings->userdata('id') ?>"&quotation_id="+ $(this).attr('data-id'), 'large')
+            // })
+            $('table').dataTable();
 
-    })
-</script>
+        })
+    </script>
