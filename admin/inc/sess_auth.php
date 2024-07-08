cd17b4144de4514c3643d6f6ca542adab4e23e7a
Vikas Kumar <51703051+vikas9238@users.noreply.github.com>
2024-07-09 01:06:57 +0530

    Update sess_auth.php

--- admin/inc/sess_auth.php
+++ admin/inc/sess_auth.php
@@ -12,11 +12,11 @@ $link .= $_SERVER['REQUEST_URI'];
 if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
 	redirect('admin/login.php');
 }
-if(isset($_SESSION['userdata']) && strpos($link, 'login.php') && $_SESSION['userdata']['login_type'] ==  1){
+if(isset($_SESSION['userdata']) && strpos($link, 'login.php') && $_SESSION['userdata']['loggedin'] ==  1){
 	redirect('admin/index.php');
 }
 $module = array('','admin');
-if(isset($_SESSION['userdata']) && (strpos($link, 'index.php') || strpos($link, 'admin/')) && $_SESSION['userdata']['login_type'] !=  1){
+if(isset($_SESSION['userdata']) && (strpos($link, 'index.php') || strpos($link, 'admin/')) && $_SESSION['userdata']['loggedin'] !=  1){
 	echo "<script>alert('Access Denied!');location.replace('".base_url.$module[$_SESSION['userdata']['login_type']]."');</script>";
     exit;
 }
