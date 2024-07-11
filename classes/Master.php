592cbe1540527665725dc102e2d67399f1839942
vikas9238 <vikashkumar9238@gmail.com>
2024-07-12 02:25:09 +0530

    a

--- classes/Master.php
+++ classes/Master.php
@@ -484,7 +484,8 @@ class Master extends DBConnection
 			} elseif ($status == 4) {
 				$order = $this->conn->query("SELECT quantity FROM `booking_list` where id = '{$id}'")->fetch_array()['quantity'];
 				if ($approved_quantity <= $order) {
-					$remaining_quantity = $order - $approved_quantity;
+					$sell = $this->conn->query("SELECT approved_quantity FROM `booking_list` where id = '{$id}'")->fetch_array()['approved_quantity'];
+					$remaining_quantity = $order - ($approved_quantity + $sell);
 					$date = date('Y-m-d H:i:sa');
 					$sql = "UPDATE `booking_list` set approved_quantity = approved_quantity + '{$approved_quantity}', status='4', confirm_order='{$date}' where id ='{$id}'";
 					$save = $this->conn->query($sql);
