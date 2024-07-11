f6f72ce61c1bd8bb414def7e764a38ec3af3bc06
vikas9238 <vikashkumar9238@gmail.com>
2024-07-11 21:41:13 +0530

    a

--- mail/welcome.php
+++ mail/welcome.php
@@ -4,6 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
 require_once('../config.php');
 
 //Load Composer's autoloader
@@ -13,50 +14,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['firstname']) && !empt
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
-    $mobile=$_settings->info('mobile');
-    $company_email=$_settings->info('email');
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'no-reply@nrfindustry.in';                     //SMTP username
-    $mail->Password   = 'Nrf@9238';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mobile = $_settings->info('mobile');
+    $company_email = $_settings->info('email');
+    $logo = $_settings->info('logo');
+    $base_url = base_url;
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'no-reply@nrfindustry.in';                     //SMTP username
+        $mail->Password   = 'Nrf@9238';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('no-reply@nrfindustry.in', 'NRF INDUSTRY');
-    $mail->addAddress($email);     //Add a recipient
- 
-
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Account registration has been successfully approved!';
-    $mail->Body    = "Dear $firstname $lastname,
-
-    <p>We are delighted to inform you that your account with NRF Industry And Trading Private Limited has been approved and is now active!</p>
-
-    <p>You can now access all the features and benefits of our platform. Please log in using the credentials you provided during registration to get started.</p>
-
-    <p>If you have any questions or encounter any issues while using our platform, feel free to reach out to our support team at <b>Email:</b><a href='mailto:$company_email'> $company_email</a> <b>Contact:</b><a href='tel:$mobile'> +91-$mobile</a>. We're here to assist you.</p>
-
-    <p>Thank you for choosing NRF Industry And Trading Private Limited. We look forward to serving you.</p>
-
-    Best regards,<br>
-    NRF Industry Team";
+        //Recipients
+        $mail->setFrom('no-reply@nrfindustry.in', 'NRF INDUSTRY');
+        $mail->addAddress($email);     //Add a recipient
 
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Account registration has been successfully approved!';
+        $mail->Body    = "<body style='font-family: Arial, sans-serif;background-color: #f4f4f4;margin: 0;padding: 0;'>
+    <div style='background-color: #fff;margin: 0 auto;padding: 20px;max-width: 600px;border: 1px solid #ddd;'>
+        <div style='background-color: #00B98E;color: #fff;padding: 10px;text-align: center;'>
+            <h1><img src='$base_url$logo'>NRF INDUSTRY</h1>
+        </div>
+        <div style='padding: 20px;'>
+            <h2>Account Registration Confirmation</h2>
+            <p style='font-size: 14px;line-height: 1.6;'>Dear $firstname $lastname,</p>
+            <p style='font-size: 14px;line-height: 1.6;'>We are delighted to inform you that your account with NRF Industry And Trading Private Limited has been approved and is now active!</p>
+            <p style='font-size: 14px;line-height: 1.6;'>You can now access all the features and benefits of our platform. Please log in using the credentials you provided during registration to get started.</p>
+            <p style='color: #d9534f;font-size: 14px;line-height: 1.6;'>If you have any questions or encounter any issues while using our platform, feel free to reach out to our support team at <b>Email:</b><a href='mailto:$company_email'> $company_email</a> <b>Contact:</b><a href='tel:$mobile'> +91$mobile</a>. We're here to assist you.</p>
+            <p style='font-size: 14px;line-height: 1.6;'>Thank you for choosing NRF Industry And Trading Private Limited. We look forward to serving you.</p>
+            <p style='font-size: 14px;line-height: 1.6;'>Best Regards,<br>
+                NRF Industry Team</p>
+            <p style='font-size: 14px;line-height: 1.6;'><em>(This is a system generated mail and should not be replied to)</em></p>
+            <hr>
+            <div style='margin-top: 20px;font-size: 12px;color: #666;'>
+                <p style='font-size: 14px;line-height: 1.6;'>Do not share your login username/password via email or over the phone. NRF Industry Team will never ask for it.</p>
+                <p style='font-size: 14px;line-height: 1.6;'>*For all Term and Condition (t&c), Please refer to the Website <a href='https://nrfindustry.in/'>link</a></p>
+            </div>
+        </div>
+    </div>
+</body>";
+
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
