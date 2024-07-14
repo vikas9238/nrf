0c8105d4e126c10567c27b0b22dd9f25b5b20de5
Vikas Kumar <51703051+vikas9238@users.noreply.github.com>
2024-07-14 14:51:43 +0530

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -135,6 +135,7 @@
     </div>
     <!-- /.info-box -->
   </div>
+  <?php if($_settings->admindata('username')==admin):?>
   <div class="col-12 col-sm-6 col-md-3">
     <div class="info-box mb-3">
       <span class="info-box-icon bg-success elevation-1"><i class="fa fa-indian-rupee-sign"></i></span>
@@ -152,6 +153,7 @@
     </div>
     <!-- /.info-box -->
   </div>
+            <?php endif;?>
   <div class="col-12 col-sm-6 col-md-3">
     <div class="info-box mb-3">
       <span class="info-box-icon bg-secondary elevation-1"><i class="fa fa-indian-rupee-sign"></i></span>
